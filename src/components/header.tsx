04379500c27a81ecfaf61934a1723c6ae202a3a0
nas <y.softvalley@hey.com>
2024-09-18 22:51:50 +0900

    Headerコンポーネントでのナビゲーション方法をLinkからButtonに変更
    * 'Link'コンポーネントを使わずに、'useRouter'フックを利用したナビゲーションを実装するため
    * 遷移処理をButtonのonClickイベントにカプセル化することで、より柔軟な操作を可能にする
    * この変更により、ユーザー体験の向上を目指し、UIの一貫性を保つことができる

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,21 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
 import * as React from "react";
 import { Button } from "./ui/button";
 
 export const Header = () => {
+	const router = useRouter();
 	return (
 		<header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
 			<div className="flex items-center justify-end space-x-2">
-				<Link href="/" legacyBehavior passHref>
-					<Button variant="ghost">
-						<span className="hidden sm:block">Kolumoana Chat UI</span>
-					</Button>
-				</Link>
+				<Button
+					variant="ghost"
+					onClick={() => {
+						router.push("/");
+					}}
+				>
+					<span className="hidden sm:block">Kolumoana Chat UI</span>
+				</Button>
 			</div>
 		</header>
 	);
