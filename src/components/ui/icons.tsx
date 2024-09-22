42972915eee936bc5925fb3074037569f65046bc
nas <y.softvalley@hey.com>
2024-09-22 23:02:25 +0900

    SVGアイコンのスタイルを修正
    * より鮮明な表示を実現するために、アイコンの属性を調整しました。
    * 'fill'属性を削除し、'stroke'属性に変更することで、アイコンが不透明ではなくなり、背景とのコントラストが良好になります。
    * これにより、アイコンの視認性が向上し、デザイン全体の一貫性が保たれることを目的としています。

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -227,7 +227,9 @@ function IconKey({ className, ...props }: React.ComponentProps<"svg">) {
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 256 256"
-			fill="currentColor"
+			stroke="currentColor"
+			strokeWidth="1"
+			fill="none"
 			className={cn("size-4", className)}
 			{...props}
 		>
